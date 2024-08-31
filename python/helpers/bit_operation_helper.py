2d820400876668be8aee68384b63d966f9a2356a
Tom Smith <Tom_Esq@Yahoo.com>
2024-08-31 15:51:01 +0100

    Added some helper classes for bit and block operations

---
+++ python/helpers/bit_operation_helper.py
@@ -0,0 +1,38 @@
+BASE_2_RANGE = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+
+class BitOperationHelper():
+    def __init__(self) -> None:
+        pass
+
+    def circular_shift_left(self, input: int, shift_count: int) -> int:
+        bit_amount = self.__parse_input_bit_amount(input)
+        bit_cap_mask = self.__parse_bit_cap_mask(bit_amount)
+        
+        print(f"Input: {input} bit amount: {bit_amount}")
+
+        bitshifted_var = (input << shift_count) & bit_cap_mask
+        missing_bits = input >> bit_amount - shift_count 
+        return bitshifted_var | missing_bits
+
+    def circular_shift_right(self, input: int, shift_count: int) -> int:
+        bit_amount = self.__parse_input_bit_amount(input)
+        bit_cap_mask = self.__parse_bit_cap_mask(bit_amount)
+        
+        print(f"Input: {input} bit amount: {bit_amount}")
+        
+        bitshifted_var = input >> shift_count
+        missing_bits = input << (bit_amount - shift_count) & bit_cap_mask
+        return bitshifted_var | missing_bits
+
+    def get_next_base_2(self, var: int) -> int:
+        var -= 1
+        for b2i in BASE_2_RANGE:
+            var |= var >> b2i
+        var += 1
+        return var
+
+    def __parse_input_bit_amount(self, input: int) -> int:
+        return self.__get_next_base_2(len(bin(input)[2:]))
+
+    def __parse_bit_cap_mask(self, bitshift_amount: int) -> int:
+        return pow(2, bitshift_amount) - 1
---
+++ python/helpers/block_operation_helper.py
@@ -0,0 +1,31 @@
+DEFAULT_WORD_SIZE = 8
+DEFAULT_ENDIANNESS = 'little'
+
+class BlockOperationHelper():
+    
+    def parse_string_to_blocks(self, message: str, block_size: int) -> list[bytes]:
+        chunk_size = block_size // DEFAULT_WORD_SIZE
+        message_blocks = self.__build_default_chunks_array(message, chunk_size)
+
+        for idx, char in enumerate(message):
+            chunk_index = idx // chunk_size
+            bitmask_index = idx % chunk_size
+            message_blocks[chunk_index] |= self.__char_to_bitmask(char, bitmask_index)
+
+        return message_blocks
+
+    def parse_blocks_to_string(self, chunk_array: list[bytes]) -> str:
+        return ''.join([self.__parse_block_to_char_array(chunk) for chunk in chunk_array])
+
+    def __int_to_bytes(self, input: int, chunk_size: int) -> bytes:
+        return input.to_bytes(chunk_size, DEFAULT_ENDIANNESS)
+    
+    def __char_to_bitmask(self, char: str, bitmask_index: int) -> bytes:
+        return (ord(char) << (bitmask_index) * DEFAULT_WORD_SIZE)
+
+    def __build_default_chunks_array(self, message: str, chunk_size: int) -> list[bytes]:
+        return [0b0] * -(len(message) // -chunk_size)
+    
+    def __parse_block_to_char_array(self, block: bytes) -> str:
+        block_bytes = self.__int_to_bytes(block, block.__sizeof__())
+        return ''.join([chr(byte) for byte in block_bytes if byte != 0x0])
