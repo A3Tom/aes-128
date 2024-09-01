eb4d56dd4b19ba4a9e9ef33d65669f4b2e4812ba
Tom Smith <Tom_Esq@Yahoo.com>
2024-09-01 19:06:45 +0100

    This is truly a wild stage to get to but fuck me is it kinda a mess atm. Key Schedule, SubBytes and Shift Rows down though !!!

--- python/encryption/encryption_aes.py
+++ python/encryption/encryption_aes.py
@@ -27,22 +27,26 @@ class AES(EncryptionBase):
 
     # https://crypto.stackexchange.com/questions/20/what-are-the-practical-differences-between-256-bit-192-bit-and-128-bit-aes-enc/1527#1527
     def _encrypt_block(self, block: bytes) -> bytes:
-        # prev_block = self.__add_round_key()
+        prev_block = block
+        # prev_block = 66814286504060421741230023322616923956 # known test value
+        prev_block = self.__add_round_key(prev_block, self.key)
+        print(f"{prev_block:032X}")
 
-        # for round in self.__rounds:
-        #     round_key = round
+        for round in range(1):
+            prev_block = int.from_bytes(self.__sub_bytes(prev_block))
+            print(f"{prev_block:032X}")
 
-        #     block = self.__sub_bytes()
-        #     block = self.__shift_rows()
+            prev_block = self.__shift_rows(prev_block)
+            print(f"{prev_block:032X}")
 
-        #     if round != self.__rounds:
+
+        #     if round != (self.__rounds - 1):
         #         block = self.__mix_columns()
             
         #     block = self.__add_round_key(block, round_key)
         #     pass
         
         return block ^ self.key
-
 
     def _decrypt_block(self, block: bytes) -> bytes:
         return block ^ self.key
@@ -79,7 +83,7 @@ class AES(EncryptionBase):
             self.__round_keys[round] = round_key
             previous_block = round_key
         
-        print(f"Key: {self.key} produces expanded key schedule:")
+        print(f"Key: {self.key:032X} produces expanded key schedule:")
 
         for round in range(self.__rounds):
             print(f"R{round + 1:02}: {self.__round_keys[round]:032X}")
@@ -88,11 +92,30 @@ class AES(EncryptionBase):
     def __add_round_key(self, block: bytes, round_key: bytes):
         return block ^ round_key
 
-    def __sub_bytes(self, block: bytes):
-        return block
+    def __sub_bytes(self, block: int):
+        return [self._sub_byte(byte) for byte in self.block_op_helper.int_to_bytes(block, 16, 'big')]
 
+    def __shift_rows_array(self, block: bytes):
+        block_2darray = self.block_op_helper.build_2d_byte_array(int.from_bytes(block))
+        self.block_op_helper.output_2d_array_hex(block_2darray)
+        for i in range(4):
+            for _ in range(i):
+                block_2darray[i].append(block_2darray[i].pop(0))
+
+        self.block_op_helper.output_2d_array_hex(block_2darray)
+        return block_2darray
+    
     def __shift_rows(self, block: bytes):
-        return block
+        block_array_2d = self.block_op_helper.build_2d_byte_array(block)
+        self.block_op_helper.output_2d_array_hex(block_array_2d)
+
+        for col_idx in range(4):
+            row = int.from_bytes(block_array_2d[col_idx])
+            row = self.bit_op_helper.circular_shift_left(row, (col_idx * 8))
+            block_array_2d[col_idx] = row.to_bytes(4)
+
+        block_array_2d = self.block_op_helper.transpose_2d_array(block_array_2d)
+        return self.block_op_helper.parse_2d_byte_array_to_int(block_array_2d)
 
     def __mix_columns(self, block: bytes):
         return block
