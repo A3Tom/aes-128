48480eae47ad1035b727ff140c652bf1a2260571
Tom Smith <Tom_Esq@Yahoo.com>
2024-09-10 22:31:07 +0100

    More gmul testing

--- python/gmul_testing.py
+++ python/gmul_testing.py
@@ -5,4 +5,12 @@ boh = BitOperationHelper()
 a = 0xA2
 
 print()
-print(f"0x{a:002X} gmul2 = 0x{boh.gmul2(a):002X} | gmul3 = 0x{boh.gmul3(a):002X}")
+
+output_string = ""
+output_string += f"0x{a:002X} "
+output_string += f"gmul2 = 0x{boh.gmul(a, 2):002X} : {boh.gmul(a, 2)} | "
+output_string += f"gmul3 = 0x{boh.gmul(a, 3):002X} : {boh.gmul(a, 3)} | "
+output_string += f"gmul4 = 0x{boh.gmul(a, 4):002X} : {boh.gmul(a, 4)} | "
+output_string += f"gmul5 = 0x{boh.gmul(a, 5):002X} : {boh.gmul(a, 5)} | "
+
+print(output_string)
