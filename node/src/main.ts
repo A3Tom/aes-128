7274ac24bbfa9ec2098cee5a2f7c5f6828293782
Tom Smith <Tom_Esq@Yahoo.com>
2024-11-23 23:52:54 +0000

    Fleshed oot the first part of the key schedule and made a call to the setup output method

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,11 +1,50 @@
+import { calculateEncryptionRounds } from "./lib/aes-utils";
+import { buildBitCapMask, toBinaryString } from "./lib/bit-utils";
+import { EncryptionSetup, formatSetupOutput } from "./lib/spoutin-utils";
+import { KEY_SIZE, MODE_OF_OPERATION } from "./models/aes-settings";
+
+// 0: Setup
 const message: string = "Remo";
 const key: bigint = 57811460909138771071931939740208549692n;
 
-// 0: Setup
+const setup: EncryptionSetup = {
+    message,
+    key,
+    modeOfOperation: MODE_OF_OPERATION.ECB,
+    keySize: KEY_SIZE._128
+};
 
-// Set Mode of Operation (ECB for this outline (and ma sanity ...))
+const roundKeys: Record<number, bigint> = {};
+
+formatSetupOutput(setup);
+
 // Calculate encryption rounds
+const encryptionRounds = calculateEncryptionRounds(setup.keySize);
+
 // Expand key schedule
+expandKeySchedule(key, encryptionRounds);
+
+Object.keys(roundKeys).map(roundKey => {
+    console.log(`[r-${roundKey.toString().padStart(2, '0')}k] ${toBinaryString(roundKeys[+roundKey], 16)}`);
+})
+
+
+function expandKeySchedule(key: bigint, rounds: number): void {
+    let previousBlock: bigint = key;
+    let previousColumn: bigint = previousBlock & 0xFFFFFFFFn;
+    let permutationColumn: bigint = previousColumn;
+
+    // permutationColumn = circularLeftShift(permutationColumn, 8);
+
+    for (let round = 0; round <= rounds; round++) {
+        const roundKey: number = 0x0;
+        roundKeys[round] = BigInt(1 << round);
+    }
+}
+
+
+
+
 
 // Encrypting the Message
 
