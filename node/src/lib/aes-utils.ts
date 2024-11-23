f7d5cac8e97f1a047471b9d57c3033257adea29f
Tom Smith <Tom_Esq@Yahoo.com>
2024-11-23 23:52:19 +0000

    Typescript is pretty sound man

--- node/src/lib/aes-utils.ts
+++ node/src/lib/aes-utils.ts
@@ -1,3 +1,6 @@
+import { KEY_SIZE } from "../models/aes-settings";
+
+export const WORD_SIZE = 32;
 export const BLOCK_SIZE = 128;
 export const ROUND_CONSTANTS = [
     0x01000000, 0x02000000,
@@ -5,10 +8,24 @@ export const ROUND_CONSTANTS = [
     0x10000000, 0x20000000,
     0x40000000, 0x80000000,
     0x1B000000, 0x36000000
-]
+];
 export const MIX_COLUMN_MATRIX = [
     [2, 3, 1, 1],
     [1, 2, 3, 1],
     [1, 1, 2, 3],
     [3, 1, 1, 2]
-]
+];
+
+
+export function calculateEncryptionRounds(keySize: KEY_SIZE): number {
+    switch (keySize) {
+        case KEY_SIZE._128:
+            return 10;
+        case KEY_SIZE._192:
+            return 12;
+        case KEY_SIZE._256:
+            return 14;
+        default:
+            throw `Key Size: ${keySize} isny valid ya fanny`;
+    }
+}
---
+++ node/src/models/aes-settings.ts
@@ -0,0 +1,11 @@
+export const enum MODE_OF_OPERATION {
+    ECB = "ecb",
+    CTR = "ctr",
+    GCM = "gcm"
+}
+
+export const enum KEY_SIZE {
+    _128 = "128",
+    _192 = "192",
+    _256 = "256"
+}
