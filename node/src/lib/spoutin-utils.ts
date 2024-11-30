94a2538cf9454bef3582a1cdaf2a770d74e760ff
Tom Smith <Tom_Esq@Yahoo.com>
2024-11-30 20:20:29 +0000

    Spoutin refactor

--- node/src/lib/spoutin-utils.ts
+++ node/src/lib/spoutin-utils.ts
@@ -1,24 +1,17 @@
 import { ROUND_STAGE } from "../models/aes-settings";
 import { EncryptionSetup } from "../models/setup";
 import { LOG_VERBOSITY } from "../models/system-settings";
-import { convertByteArrayToInt, toBinaryString, WORD_SIZE } from "./bit-utils";
+import { BYTE_SIZE, convertByteArrayToInt } from "./bit-utils";
 
 const logVerbosity: LOG_VERBOSITY = LOG_VERBOSITY.YAPPIN;
 
 export function formatSetupOutput(setup: EncryptionSetup) {
     console.log("************************");
     console.log(`  AES-${setup.keySize} (${setup.modeOfOperation})`);
-    console.log(`  Key: ${setup.key}\n`);
-
-    const bKey = setup.key.toString(2).padStart(+setup.keySize, '0');
-    for (let i = 0; i < bKey.length / WORD_SIZE; i++) {
-        const start: number = 0 + (WORD_SIZE * i);
-        const end: number = start + WORD_SIZE;
-
-        console.log(`\t${bKey.substring(start, end)}`);
-    }
-
-    console.log(`  Message: ${setup.message}`);
+    console.log(`  Key: ${setup.key}`);
+    console.log(`  Hex: ${toHexSplit(setup.key, BYTE_SIZE)}`);
+    console.log(`  Msg: ${setup.message}`);
+    console.log("************************\n");
 }
 
 export function toHexString(value: number | bigint | Uint8Array, keySize: number) {
@@ -36,4 +29,12 @@ export function outputVerbose(stage: ROUND_STAGE, roundIdx: number, message: str
 
 export function formatRoundKey(roundKey: number) {
     return `[r-${(roundKey + 1).toString().padStart(2, '0')}]`
+}
+
+export function toHexSplit(key: bigint, denoteIdx: number, joinStr: string = ' |') {
+    return key.toString(16).toUpperCase().split('').reduce((output, char, idx) => {
+        output += `${idx && !(idx % denoteIdx) ? joinStr : ''}`;
+        output += `${idx && !(idx % 2) ? ' ' : ''}${char}`
+        return output;
+    }, "");
 }
