a2a7f04ebd94443afdeb4f60a3a817e3751443c1
Tom Smith <Tom_Esq@yahoo.com>
2026-09-13 13:35:58 +0100

    Added a wee string formatter to help debugging

---
+++ csharp/src/StringHelpers.cs
@@ -0,0 +1,18 @@
+namespace aes128;
+
+public static class StringHelpers
+{
+    public static string CovertBytesToHexString(byte[] bytes)
+    {
+        Int128 result = 0;
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            result <<= 8; 
+            result |= bytes[i];
+        }
+        
+        string format = $"X{Math.Floor(bytes.Length / 8d) * 8}";
+        return $"{result.ToString(format)}";
+    }
+}
